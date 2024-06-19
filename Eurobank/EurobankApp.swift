c0f24463d457a29f683c442898de2b9dcf086d60
vpigadas <vassilis.pigadas@gmail.com>
2024-06-19 08:30:21 +0300

    Initial Commit

---
+++ Eurobank/EurobankApp.swift
@@ -0,0 +1,17 @@
+//
+//  EurobankApp.swift
+//  Eurobank
+//
+//  Created by Vasileios Pigadas on 19/6/24.
+//
+
+import SwiftUI
+
+@main
+struct EurobankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
